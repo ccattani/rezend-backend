65796de6807ec33f9f77841a6d4334193b251801
ccattani <cheycattani@gmail.com>
2026-02-28 21:42:47 -0300

    Testando

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerSpec = require('./swagger')
+// const swaggerUi = require('swagger-ui-express')
+// const swaggerSpec = require('./swagger')
 
 const authRoutes = require('./routes/auth.routes')
 const vehicleRoutes = require('./routes/vehicle.routes')
@@ -18,7 +18,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.json({ message: 'API Rezende Veículos funcionando' }))
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use('/auth', authRoutes)
 app.use('/vehicles', vehicleRoutes)
