b2bb59f8144ae1a19bb81e168e71f68d2dcb6bf7
ccattani <cheycattani@gmail.com>
2026-03-01 11:59:56 -0300

    Revert "Ajustes ára registrar usuário"
    This reverts commit c8924d09f64d7d98b424a3005c9e1790c53ec435.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -47,9 +47,10 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth.controller')
+const { verifyToken } = require('../middleware/auth.middleware')
+const { verifyPermission } = require('../middleware/permission.middleware')
 
-// público
-router.post('/register', authController.register)
+router.post('/register', verifyToken, verifyPermission('CREATE_USER'), authController.register)
 router.post('/login', authController.login)
 
 module.exports = router
