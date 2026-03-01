47181447d708436d0af6125094937a93a73f6711
ccattani <cheycattani@gmail.com>
2026-03-01 11:52:14 -0300

    Revert "Ajuste para o front"
    This reverts commit 3929869d3ff1bf20b73e39ef4a3178fcd6e908c9.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -47,11 +47,9 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth.controller')
-const { verifyToken } = require('../middleware/auth.middleware')
 
 // público
 router.post('/register', authController.register)
 router.post('/login', authController.login)
-router.get('/me', verifyToken, authController.me)
 
 module.exports = router
